9dddf424c31505ee809551db5d39da851c578517
Irorll <aapoqvick@gmail.com>
2024-03-27 09:12:54 +0200

    added echo

--- poytakone/poytakone.sh
+++ poytakone/poytakone.sh
@@ -15,4 +15,6 @@ echo "w /sys/class/graphics/fbcon/cursor_blink - - - - 0" | sudo tee /etc/tmpfil
 
 pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr irssi evince steam pavucontrol qbittorrent speedtest-cli nethack feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv obs-studio audacity tenacity base-devel git links dosbox pamixer innoextract acpi --noconfirm
 
+echo "All installed. Clean cashe?"
+
 pacman -Scc
