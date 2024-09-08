8496c91a045e8b7bf90bb596578dc2f5ab637d1b
Irorll <aapoqvick@gmail.com>
2024-09-08 19:40:06 +0300

    added imagemagick and feh

--- tyokone/tyokone.sh
+++ tyokone/tyokone.sh
@@ -13,7 +13,10 @@ cp vimrc /etc/
 
 echo "w /sys/class/graphics/fbcon/cursor_blink - - - - 0" | sudo tee /etc/tmpfiles.d/cursor_blink.conf
 
-pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr evince pavucontrol speedtest-cli feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv base-devel git links pamixer innoextract acpi flatpak fastfetch --noconfirm
+pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr evince pavucontrol speedtest-cli feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv base-devel git links pamixer innoextract acpi flatpak fastfetch imagemagick --noconfirm
+
+magick -size 1920x1920 xc:#000000 /home/USER/.black.png
+feh --bg-scale /home/$USER/.black.png
 
 rm /etc/xdg/picom.conf
 cp picom.conf /etc/xdg
