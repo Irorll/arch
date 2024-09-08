54c63be4455b8abb5b767bfa864765e53edab09c
Irorll <aapoqvick@gmail.com>
2024-09-08 19:25:41 +0300

    added magick and feh

--- lappari/lappari.sh
+++ lappari/lappari.sh
@@ -28,7 +28,10 @@ chmod +x /home/$USER/.wificonnect5.sh
 
 echo "w /sys/class/graphics/fbcon/cursor_blink - - - - 0" | sudo tee /etc/tmpfiles.d/cursor_blink.conf
 
-pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr irssi evince steam pavucontrol qbittorrent speedtest-cli nethack feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv obs-studio audacity tenacity base-devel git links dosbox pamixer innoextract acpi flatpak fastfetch --noconfirm
+pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr irssi evince steam pavucontrol qbittorrent speedtest-cli nethack feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv obs-studio audacity tenacity base-devel git links dosbox pamixer innoextract acpi flatpak fastfetch imagemagick --noconfirm
+
+magick -size 1920x1920 xc:#000000 /home/$USER/.black.png
+feh --bg-scale /home/$USER/.black.png
 
 rm /etc/xdg/picom.conf
 cp picom.conf /etc/xdg/
