844fac093400de32d4b6b989f147195ab6fad0f4
Irorll <aapoqvick@gmail.com>
2024-09-09 14:37:41 +0300

    tsetis

--- lappari/lappari.sh
+++ lappari/lappari.sh
@@ -30,7 +30,7 @@ echo "w /sys/class/graphics/fbcon/cursor_blink - - - - 0" | sudo tee /etc/tmpfil
 
 pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr irssi evince steam pavucontrol qbittorrent speedtest-cli nethack feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv obs-studio audacity tenacity base-devel git links dosbox pamixer innoextract acpi flatpak fastfetch imagemagick --noconfirm
 
-magick -size 1920x1920 xc:#000000 /home/$USER/.black.png
+magick -size 1920x1920 xc:#009900 /home/$USER/.black.png
 feh --bg-scale /home/$USER/.black.png
 
 rm /etc/xdg/picom.conf
