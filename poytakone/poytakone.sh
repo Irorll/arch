cd4f1b44002e942f6c255a546abd1f682a214897
irorll <basisti888@hotmail.com>
2024-10-05 13:51:02 +0300

    added flameshot install

--- poytakone/poytakone.sh
+++ poytakone/poytakone.sh
@@ -13,7 +13,7 @@ cp vimrc /etc/
 
 echo "w /sys/class/graphics/fbcon/cursor_blink - - - - 0" | sudo tee /etc/tmpfiles.d/cursor_blink.conf
 
-pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr irssi evince steam pavucontrol qbittorrent speedtest-cli nethack feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv obs-studio audacity tenacity base-devel git links dosbox pamixer innoextract acpi flatpak fastfetch imagemagick android-file-transfer nemo psensor --noconfirm
+pacman -Syu firefox vim redshift caja dosfstools ntfs-3g less arandr irssi evince steam pavucontrol qbittorrent speedtest-cli nethack feh htop neofetch flatpak picom gnome-disk-utility calcurse vlc yt-dlp ytfzf fzf mpv obs-studio audacity tenacity base-devel git links dosbox pamixer innoextract acpi flatpak fastfetch imagemagick android-file-transfer nemo psensor flameshot --noconfirm
 
 magick -size 1920x1920 xc:#000000 /home/$USER/.black.png
 feh --bg-scale /home/$USER/.black.png
